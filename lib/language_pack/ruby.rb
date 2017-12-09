2ff170c62feff2fbe80ee3afee88a740fcc769f1
Mike Dalpee <mikedalpee@gmail.com>
2017-12-09 16:10:03 -0500

    Hope

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -615,7 +615,8 @@ WARNING
             "BUNDLE_DISABLE_VERSION_CHECK"  => "true",
             "QT_DIR"                        => "#{ARGV[0]}/.apt/usr/lib/x86_64-linux-gnu/qt5/",
             "QMAKEPATH"                     => "#{ARGV[0]}/.apt/usr/lib/x86_64-linux-gnu/qt5/",
-            "QMAKESPEC"                     => "#{ARGV[0]}/.apt/usr/lib/x86_64-linux-gnu/qt5//mkspecs/linux-g++/"
+            "QMAKESPEC"                     => "#{ARGV[0]}/.apt/usr/lib/x86_64-linux-gnu/qt5//mkspecs/linux-g++/",
+            "QT_MAJOR_VERSION"              => "5"
           }
           env_vars["BUNDLER_LIB_PATH"] = "#{pwd}/#{bundler_path}/lib" if ruby_version.ruby_version == "1.8.7"
           puts "Running: #{bundle_command}"
