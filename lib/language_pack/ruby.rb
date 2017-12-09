d36d1d7696609393b188f7e7cfbbeb46233fb430
Mike Dalpee <mikedalpee@gmail.com>
2017-12-09 18:00:39 -0500

    Hope

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -606,14 +606,13 @@ WARNING
           env_vars       = {
             "BUNDLE_GEMFILE"                => "#{pwd}/Gemfile",
             "BUNDLE_CONFIG"                 => "#{pwd}/.bundle/config",
-            "CPATH"                         => noshellescape("#{yaml_include}:#{ARGV[0]}/.apt/usr/include:#{ARGV[0]}/.apt/usr/include/x86_64-linux-gnu/qt5:$CPATH"),
-            "CPPATH"                        => noshellescape("#{yaml_include}:#{ARGV[0]}/.apt/usr/include:#{ARGV[0]}/.apt/usr/include/x86_64-linux-gnu/qt5:$CPPATH"),
+            "CPATH"                         => noshellescape("#{yaml_include}:$CPATH"),
+            "CPPATH"                        => noshellescape("#{yaml_include}:$CPPATH"),
             "LIBRARY_PATH"                  => noshellescape("#{yaml_lib}:$LIBRARY_PATH"),
             "RUBYOPT"                       => syck_hack,
             "NOKOGIRI_USE_SYSTEM_LIBRARIES" => "true",
             "JAVA_HOME"                     => noshellescape("#{pwd}/$JAVA_HOME"),
-            "BUNDLE_DISABLE_VERSION_CHECK"  => "true",
-            "INCLUDE_PATH"                  => noshellescape("#{ARGV[0]}/.apt/usr/include:#{ARGV[0]}/.apt/usr/include/x86_64-linux-gnu/qt5:$INCLUDE_PATH")
+            "BUNDLE_DISABLE_VERSION_CHECK"  => "true")
           }
           env_vars["BUNDLER_LIB_PATH"] = "#{pwd}/#{bundler_path}/lib" if ruby_version.ruby_version == "1.8.7"
           puts "Running: #{bundle_command}"
